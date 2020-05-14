6194fb69601a6ab939616b188999fdcc20364f18
Ubuntu <ubuntu@ip-172-31-16-231.ec2.internal>
2020-05-14 14:13:11 +0000

    minor change in gpus available

--- src/models/core/experiments.py
+++ src/models/core/experiments.py
@@ -103,7 +103,7 @@ def run_param_sweep(base_cmd, grid, ngpus_per_run=1,
     system_gpus = GPUtil.getGPUs()
     system_gpu_ids = [gpu.id for gpu in system_gpus]
     available_gpu_ids = get_available_GPUs(free_gpu_max_mem)
-    available_gpu_ids = [id for id in available_gpu_ids if id in range(7)]
+    available_gpu_ids = [id for id in available_gpu_ids if id in range(8)]
     # available_gpu_ids = [id for id in available_gpu_ids if id in gpus_to_use]
     n_available = len(available_gpu_ids)
 
